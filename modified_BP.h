abb419719aa7556a52f44c0cd4529e3fb12dbbbb
XR Liu <41162248+LiuXingrui@users.noreply.github.com>
2022-10-28 12:11:09 -0700

    Update modified_BP.h

--- modified_BP.h
+++ modified_BP.h
@@ -32,9 +32,19 @@ using namespace itpp;
 \param num_of_row_reduction is the number of how many rows are reduced.
  */
 //
-void Mat_Trans(GF2mat& H, GF2mat& D, GF2mat& H_tilde, GF2mat& D_tilde, vector<vector<int>>& A, int num_of_row_reduction,int debug=0);
+void Mat_Trans(GF2mat& H, GF2mat& D, vector<double> &K, vector<double>& K_tilde, GF2mat& H_tilde, GF2mat& D_tilde, vector<vector<int>>& A, int num_of_row_reduction,int debug=0);
 
 
-void row_reduction(int which_row, GF2mat& H, GF2mat& D, GF2mat& H2, GF2mat& D2, vector<vector<int>>& A, int debug=0);
+void row_reduction(int which_row, GF2mat& H, GF2mat& D, GF2mat& H2, GF2mat& D2, vector<double> &input_K,vector<double>&output_K,vector<vector<int>>& A, int debug=0);
 
-void Add_cols (GF2mat& H,GF2mat& D,vector<int>& Ai,const int w,int debug=0);
+void construct_B_tau(vector<double> &B_tau,int w, vector<double>& input_K, vector<int> Ai,vector<vector<int>>&  Tau);
+
+void Add_cols(GF2mat& H,GF2mat& D,vector<int>& Ai,const int w,int debug=0);
+
+void K_trans(vector<double>& input_K, vector<double> &output_K, vector<int>& Ai, int w,vector<vector<int>>& b);
+
+void Add_cols_and_rows(GF2mat& H,GF2mat& D,vector<int>& Ai,int w,GF2mat& H2,GF2mat& D2,int debug,vector<vector<int>>& b);
+
+void construct_BF(GF2mat& D,vector<int> &Ai, int w,int c, GF2mat& DB, GF2mat& F,vector<vector<int>>& b);
+
+GF2mat merge_mat_vert(const GF2mat &up,const GF2mat &bottom, int debug=0);
